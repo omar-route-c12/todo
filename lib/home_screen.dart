3f98e4d58535ce9a577c333b06b2fbb3e66214c0
Omar Assem <omarassem.route@gmail.com>
2024-10-13 22:32:24 +0300

    Finish home screen

---
+++ lib/home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:todo/app_theme.dart';
+import 'package:todo/tabs/settings/settings_tab.dart';
+import 'package:todo/tabs/tasks/tasks_tab.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = '/home';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Widget> tabs = [
+    TasksTab(),
+    SettingsTab(),
+  ];
+  int currentTabIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[currentTabIndex],
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        padding: EdgeInsets.zero,
+        color: AppTheme.white,
+        child: BottomNavigationBar(
+          currentIndex: currentTabIndex,
+          onTap: (index) => setState(() => currentTabIndex = index),
+          elevation: 0,
+          items: [
+            BottomNavigationBarItem(
+              label: 'Tasks',
+              icon: Icon(
+                Icons.list,
+                size: 32,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(
+                Icons.settings,
+                size: 32,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          size: 32,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
