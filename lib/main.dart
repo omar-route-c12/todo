dce570e8bc70b3c01c4e656c6941f65bbbe944be
Omar Assem <omarassem.route@gmail.com>
2024-10-23 22:24:30 +0300

    Integrate with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/app_theme.dart';
 import 'package:todo/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(TodoApp());
 }
 
